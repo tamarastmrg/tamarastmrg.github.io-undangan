c654825e71d8e2c542b15265663196161a7329e8
tamarastmrg <111110447+tamarastmrg@users.noreply.github.com>
2022-08-19 14:02:12 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+function _Swap() {
+  document.querySelector(".backgroundDiv").style.background =
+    document.querySelector(".backgroundDiv").style.background == "rgb(0, 0, 0)"
+      ? "rgb(255, 255, 255)"
+      : "rgb(0, 0, 0)";
+}
